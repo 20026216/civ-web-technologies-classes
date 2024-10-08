e365d5d0009cd532cdaea9b64339c81dfe2cc173
20026216 <20026216@tafe.wa.edu.au>
2024-10-08 10:24:16 +0800

    session 12 prog

--- session-12/assets/js_starter/scripts_challenge_1.js
+++ session-12/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,16 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
-
+const numbers1 = numbers.pop()
+console.log(numbers1);
 // Use the unshift() method to add the last element to the beginning of the array
-
+numbers.unshift()
+console.log(numbers);
 // Use the slice() method to exclude the first 3 elements and return a new array
-
+numbers.slice(0,3)
+console.log(numbers);
 // Set the value of the outputfield inner html to the new array
+let outfield = document.getElementById("OutputField");
+// outfield.innerHTML = newArray;
+let newArray = outfield.innerHTML;
+console.log(newArray);
--- session-12/assets/js_starter/scripts_challenge_3.js
+++ session-12/assets/js_starter/scripts_challenge_3.js
@@ -3,11 +3,12 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Check if the array includes the number 5
-
+let a = numbers.includes(5)
 // If true, filter out numbers less than 5
-
+let b = numbers.filter(item=> item < 5)
 // Then square each number in the resulting array
-
+let c = b.map(item=> Math.pow(item, 2))
 // Output the final array
-
+console.log(c)
 // Output a message if the number 5 is not in the array
+console.log('the number ' + 5 + ' is not in the array')
