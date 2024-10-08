eb80e1df9c3a293284724d44e99bb8773d926a21
20026216 <20026216@tafe.wa.edu.au>
2024-10-08 11:11:26 +0800

    session 12 more prog

--- session-12/assets/js_starter/scripts_challenge_4.js
+++ session-12/assets/js_starter/scripts_challenge_4.js
@@ -3,9 +3,15 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
-
+itemindex = numbers.findIndex(number => number === 5);
 // If the number 5 is found in the array
-
+if (numbers.includes(5)) {
+    nums = numbers.slice(0, itemindex + 1);
+    nums.forEach(num=> {console.log(num)})
+}
 // Console log each number in the array up to and including 5
 
 // Output a message if the number 5 is not in the array
+if (!numbers.includes(5)) {
+    console.log("yo num 5 doesn't exist");
+}
